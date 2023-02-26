da63afdae82cc68e3956e548fe8c86b169a1ae35
AhmadAbdullah22 <ahmadabdullahbrw@gmail.com>
2023-02-26 23:36:51 +0500

    Ah.py

---
+++ Ah.py
@@ -0,0 +1,32 @@
+# name = input()
+# print(f'hello, {name}')
+
+# x = 28
+
+# if x > 0:
+#     print('x is positive')
+# elif x <0:
+#     print('x is negative')
+# else:
+#     print('x is zero')
+
+# name = "Ahamd"
+# coordiinates = (10.0, 20.0) 
+# names = ["Ahmad", "Abdullah", "Hammad"]
+
+
+# name = "Ahmad"
+# name =name[0]
+# s = set()
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(4)
+# s.add(5)
+# print(s)
+
+def square(x):
+    return x*x
+
+for i in range (10):
+    print("{} square of {}".format(i, square(i)))
