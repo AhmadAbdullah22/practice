9730503ba69d5bb3ddeb64cd95fc7fa9a4cec310
AhmadAbdullah22 <ahmadabdullahbrw@gmail.com>
2023-02-27 23:17:33 +0500

    D.py

--- D.py
+++ D.py
@@ -1,8 +1,14 @@
-class point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-p = point(3, 5)
-print(p.x)
-print(p.y)
+# p = point(3, 5)
+# print(p.x)
+# print(p.y)
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "My name is Ahmad."
