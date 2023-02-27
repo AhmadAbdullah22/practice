ae791a1ce8bebc4b4b855c4ac421cf3ca0228b29
AhmadAbdullah22 <ahmadabdullahbrw@gmail.com>
2023-02-28 00:21:05 +0500

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello, World!</h1>"
+
+
+@app.route("/<string:name>")
+
+def Hello(name): 
+    name = name.capitalize()
+    return f"<h1>Hello, {name}</h1>"
